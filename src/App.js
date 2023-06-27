2a29886703cc372cd6052b9b90c26f15c7e2a84d
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-27 14:20:02 +0100

    half-way through adding handleItemInput

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const items [items, setItems] = useState([
+  const [items, setItems] = useState([
     {name: 'Go to the shop', isComplete: false},
     {name: 'Buy socks', isComplete: false},
     {name: 'Get a cat', isComplete: false},
   ]);
   const [newItem, setNewItem] = useState("");
 
+  const itemNodes = items.map((item, index) => {
+    return(
+      <li key = {index} className={item.isCompleted ? "completed" : "not-completed"}>
+        <span> {item.name} </span>
+        {item.isPurchased ? <span className='completed'> Completed! </span> :
+        <button onClick={() => purchaseItem(index)}> Complete</button>} </li>
+    )
+    });
+    
+    const handleItemInput = (event) => {
+      setNewItem(event.target.value)
+    }
+  
   return (
     <div className="App">
 
