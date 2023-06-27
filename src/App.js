bb1c909e36ad1ea8ed7cf58ba17e0cf44be2891f
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-27 14:55:25 +0100

    finished mvp. Need to style

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import React, { useState } from 'react';
 function App() {
 
   const [tasks, setTasks] = useState([
-    {name: 'Go to the shop', isCompleted: false},
-    {name: 'Buy socks', isCompleted: false},
-    {name: 'Get a cat', isCompleted: false},
+    {name: 'Go to the shop', priority: 'low', isCompleted: false},
+    {name: 'Buy socks', priority: 'low', isCompleted: false},
+    {name: 'Get a cat', priority: 'high', isCompleted: false},
   ]);
   const [newtask, setNewTask] = useState("");
+  const [priority, setNewPriority] = useState('low');
 
   const taskNodes = tasks.map((task, index) => {
     return(
@@ -23,10 +24,14 @@ function App() {
       setNewTask(event.target.value)
     }
 
+    const handlePriorityChange = (event) => {
+      setNewPriority(event.target.value);
+    };
+
     const saveNewTask = (event) => {
       event.preventDefault();
       const copyTasks = [...tasks]
-      copyTasks.push({name:newtask, isCompleted : false})
+      copyTasks.push({name:newtask, priority: priority, isCompleted : false})
       setTasks(copyTasks)
       setNewTask("");
       
@@ -42,18 +47,18 @@ function App() {
     <div className="App">
 
       <h1>To Do List</h1>
-      <hr></hr>
-
-      <ul>
-        {taskNodes}
-      </ul>
-
       <form onSubmit={saveNewTask}>
         <label htmlFor = 'new-task'> Add new task</label>
         <input id='new-task' type='text' value={newtask} onChange={handleTaskInput}></input>
         <input type='submit' value='Save New Task'/>
-      
+        
+        <input type="radio" value="high" checked={priority === 'high'} onChange={handlePriorityChange} /> high
+        <input type="radio" value="low" checked={priority === 'low'} onChange={handlePriorityChange} /> low
       </form>
+
+      <ul>
+        {taskNodes}
+      </ul>
 
     </div>
   );
